e2b611c8b2603a889dce3c21579473e64cf2e702
chad <chad@chadduffey.com>
2019-11-25 17:52:40 -0800

    refactor before things get out of hand

---
+++ roastie/Menus.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roastie
+{
+    public class Menus
+    {
+        public static bool MenuMain()
+        {
+            displayBanner();
+            Console.Write("\n\nShould we include computer account SPNs? [y/N]: ");
+            string choice = Console.ReadLine();
+            if (choice == "y" || choice == "Y")
+            {
+                Console.Clear();
+                Console.WriteLine("[*] Searching for User and Computer SPN's: ");
+                return true;
+            }
+            Console.Clear();
+            Console.WriteLine("[*] Searching for User SPN's: ");
+            return false;
+        }
+
+        public static void displayBanner()
+        {
+            string banner = @"
+ _____                 _   _      
+|  __ \               | | (_)     
+| |__) |___   __ _ ___| |_ _  ___ 
+|  _  // _ \ / _` / __| __| |/ _ \
+| | \ \ (_) | (_| \__ \ |_| |  __/
+|_|  \_\___/ \__,_|___/\__|_|\___|";
+            Console.WriteLine(banner);
+        }
+
+        public static int MenuSPNtoRoast()
+        {
+            string choice;
+            do
+            {
+                Console.Write("\n\nWhich SPN should we fire up? ('0' for ALL): ");
+                choice = Console.ReadLine();
+                Console.Clear();
+            } while (choice != "a" && choice != "s");
+            return 1;
+        }
+
+
+    }
+}
--- roastie/Program.cs
+++ roastie/Program.cs
@@ -15,41 +15,13 @@ namespace roastie
 
         static void Main(string[] args)
         {
-            bool mode = MainMenu();
+            bool mode = Menus.MenuMain();
 
             List<string> SPNs = ListSPN(mode);
 
             DisplaySPNs(SPNs);
 
-            int target = MenuSPNtoRoast();
-        }
-
-        static bool MainMenu()
-        {
-            displayBanner();
-            Console.Write("\n\nShould we include computer account SPNs? [y/N]: ");
-            string choice = Console.ReadLine();
-            if (choice == "y" || choice == "Y")
-            {
-                Console.Clear();
-                Console.WriteLine("[*] Searching for User and Computer SPN's: ");
-                return true;
-            }
-            Console.Clear();
-            Console.WriteLine("[*] Searching for User SPN's: ");
-            return false;
-        }
-
-        static int MenuSPNtoRoast()
-        {
-            string choice;
-            do
-            {
-                Console.Write("\n\nWould you like to roast a specific SPN [s] or all SPNs [a]?: ");
-                choice = Console.ReadLine();
-                Console.Clear();
-            } while (choice != "a" && choice != "s");
-            return 1;
+            int target = Menus.MenuSPNtoRoast();
         }
 
         static List<string> ListSPN(bool include_computers)
@@ -99,16 +71,7 @@ namespace roastie
             }
         }
 
-        static void displayBanner()
-        {
-            string banner = @" _____                 _   _      
-|  __ \               | | (_)     
-| |__) |___   __ _ ___| |_ _  ___ 
-|  _  // _ \ / _` / __| __| |/ _ \
-| | \ \ (_) | (_| \__ \ |_| |  __/
-|_|  \_\___/ \__,_|___/\__|_|\___|";
-            Console.WriteLine(banner);
-        }
+        
 
     }
 }
