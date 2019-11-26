f45b5eaded5890db955d526218d1ad12c9ff4dae
chad <chad@chadduffey.com>
2019-11-25 17:58:53 -0800

    further refactor

--- roastie/Program.cs
+++ roastie/Program.cs
@@ -10,56 +10,18 @@ namespace roastie
     class Program
     {
 
-        const string LDAP_QUERY_NO_COMPUTERS      = "(&(servicePrincipalName=*/*)(!(objectClass=computer)))";
-        const string LDAP_QUERY_WITH_COMPUTERS    = "(servicePrincipalName=*/*)";
-
         static void Main(string[] args)
         {
             bool mode = Menus.MenuMain();
 
-            List<string> SPNs = ListSPN(mode);
+            List<string> SPNs = SPNSearch.GatherSPNs(mode);
 
             DisplaySPNs(SPNs);
 
             int target = Menus.MenuSPNtoRoast();
         }
 
-        static List<string> ListSPN(bool include_computers)
-        {
-            List<string> SPNs = new List<string>();
-
-            string ldapfilter;
-            if (include_computers)
-            {
-                ldapfilter = LDAP_QUERY_WITH_COMPUTERS;
-            } else
-            {
-                ldapfilter = LDAP_QUERY_NO_COMPUTERS;
-            }
-            
-
-            System.DirectoryServices.DirectoryEntry domain = new DirectoryEntry();
-            System.DirectoryServices.DirectorySearcher searcher = new DirectorySearcher();
-            searcher.SearchRoot = domain;
-            searcher.PageSize = 1000;
-            searcher.Filter = ldapfilter;
-
-            //search
-            SearchResultCollection results = searcher.FindAll();
-
-            foreach (SearchResult result in results)
-            {
-                DirectoryEntry account = result.GetDirectoryEntry();
-
-                foreach (string spn in account.Properties["servicePrincipalName"])
-                {
-                    SPNs.Add(spn);
-                }
-
-            }
-
-            return SPNs;
-        }
+        
 
         static void DisplaySPNs(List<string> SPNs)
         {
---
+++ roastie/SPNSearch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+
+namespace roastie
+{
+    class SPNSearch
+    {
+        const string LDAP_QUERY_NO_COMPUTERS = "(&(servicePrincipalName=*/*)(!(objectClass=computer)))";
+        const string LDAP_QUERY_WITH_COMPUTERS = "(servicePrincipalName=*/*)";
+
+        internal static List<string> GatherSPNs(bool include_computers)
+        {
+            List<string> SPNs = new List<string>();
+
+            string ldapfilter;
+            if (include_computers)
+            {
+                ldapfilter = LDAP_QUERY_WITH_COMPUTERS;
+            }
+            else
+            {
+                ldapfilter = LDAP_QUERY_NO_COMPUTERS;
+            }
+
+
+            System.DirectoryServices.DirectoryEntry domain = new DirectoryEntry();
+            System.DirectoryServices.DirectorySearcher searcher = new DirectorySearcher();
+            searcher.SearchRoot = domain;
+            searcher.PageSize = 1000;
+            searcher.Filter = ldapfilter;
+
+            //search
+            SearchResultCollection results = searcher.FindAll();
+
+            foreach (SearchResult result in results)
+            {
+                DirectoryEntry account = result.GetDirectoryEntry();
+
+                foreach (string spn in account.Properties["servicePrincipalName"])
+                {
+                    SPNs.Add(spn);
+                }
+
+            }
+
+            return SPNs;
+        }
+    }
+}
